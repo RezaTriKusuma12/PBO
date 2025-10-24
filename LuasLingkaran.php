c695642329eaa50e16a8ca1fcb1266c514079ddd
RezaTriKusuma12 <if24.rezakusuma@mhs.ubpkarawang.ac.id>
2025-10-24 13:57:46 +0700

    pertemuan5

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -6,6 +6,10 @@ class LuasLingkaran {
     public const phi = 3.14;
     public int $jari;
 
+    public function __construct($isiJari =1) {
+        $this ->jari = $isiJari ;
+    }
+
     public function tampil($nama){
         $rumus = LuasLingkaran:: phi *($this->jari * $this->jari) ;
         echo "Lingkaran {$nama} hasilnya Adalah :  {$rumus}";
@@ -16,9 +20,14 @@ class LuasLingkaran {
         echo"<br>";
         echo " ini dari static";
     }
+
+    public function __destruct () {
+        echo "<br>";
+        echo "udah ah cape";
+    }
 }
-$Lingkaran = new LuasLingkaran();
-$Lingkaran->jari = 12;
+$Lingkaran = new LuasLingkaran(12);
+//$Lingkaran->jari = 12;
 $Lingkaran-> tampil('bola');
 
 LuasLingkaran :: testing();
